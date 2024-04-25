52a345b094bf5fc8ee864c80fac9560434cd4b7e
Kamel Lotfi <lotfi.kamel.web@gmail.com>
2024-04-25 12:16:12 +0100

    end

---
+++ public/assets/Image-64642698.js
@@ -0,0 +1,1 @@
+import{j as n,o as i}from"./index-8aeefa04.js";const x=({src:o,className:e,alt:s="",...m})=>{const t=()=>{i({images:[o],index:0})};return n.jsx("img",{onClick:t,src:o,alt:s,className:e})};export{x as I};
---
+++ public/assets/LogoutPage-f3d6bab0.js
@@ -0,0 +1,1 @@
+import{a,b as r,r as s}from"./index-8aeefa04.js";const g=()=>{const t=a(),e=r(o=>o.logout);return s.useEffect(()=>{e(),t("/login",{replace:!0})},[]),null};export{g as default};
